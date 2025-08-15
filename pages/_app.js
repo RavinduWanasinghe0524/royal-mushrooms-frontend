5c5081d67ec6218e41ef364d7248aad6ac51508a
Ravindu Wanasinghe <ravinduwanasinghe97@gmail.com>
2025-08-15 11:25:11 +0530

    Initial commit: Royal Mushrooms Next.js frontend website

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
-import "@/styles/globals.css";
+// pages/_app.js
+import '../styles/globals.css';
+import Head from 'next/head';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+export default MyApp;
